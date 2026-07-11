a013db620387f570ba41a9cf667132cce7205575
Hitarth <avii8604900590@gmail.com>
2026-07-11 23:38:09 +0530

    Create 088_merge_sorted_array.cpp

---
+++ C++/088_merge_sorted_array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> v;
+
+        for(int i = 0; i < m; i++)
+            v.push_back(nums1[i]);
+
+        for(int i = 0; i < n; i++)
+            v.push_back(nums2[i]);
+
+        sort(v.begin(), v.end());
+
+        nums1.clear();
+
+        for(auto it : v)
+            nums1.push_back(it);
+    }
+};
